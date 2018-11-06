e0646028f3fa82797be38f938d40c5e9370fb4b1
Cindy Lee <cindylee0113@gmail.com>
2018-11-06 13:51:15 -0500

    Cleaning up code

--- a_star.py
+++ a_star.py
@@ -33,13 +33,17 @@ class TileBoard():
             for col in range(0, 3):
                 if goal_state.board[row][col] != 0:
                     goal[goal_state.board[row][col]] = (row, col)
+        # sum of Manhattan distances
         total = 0
+        # compare current element locations to the location in tuple
+        # retrieve the difference
         for row_other in range(0, 3):
             for col_other in range(0, 3):
                 if self.board[row_other][col_other] != 0:
                     num = self.board[row_other][col_other]
                     x, y = goal[num]
                     total += abs(row_other - x) + abs(col_other - y)
+        # return the sum
         return total
 
     # check if state is goal state
@@ -70,64 +74,76 @@ def make_move(tile, explored, frontier):
 	return False
 
 def search(initial, goal):
+	# initilize frontier to have initial state
+	# generated = 1 to include root node
+	# explored is currently empty
     frontier = [initial]
     explored = []
     generated = 1
+
+    # loop do
     while True:
+    	# if frontier is empty, return None as node
         if len(frontier) == 0:
-            return (0, None, "")
+            return None, generated
+        # pop off node with least function value
         node = heapq.heappop(frontier)
+        # check if node is goal node
+        # if goal node, return the node and number of generated nodes
         if node.is_goal(goal):
             return node, generated
+        # otherwise, add node to explored 
         explored.append(node)
+
+        # create left, right, up, down positions of blank space for next move
         left = (node.pos[0], node.pos[1] - 1)
         right = (node.pos[0], node.pos[1] + 1)
         up = (node.pos[0] - 1, node.pos[1])
         down = (node.pos[0] + 1, node.pos[1])
+
+        # if left position of next blank space is valid
         if left[1] != -1: 
             board = copy.deepcopy(node.board)
             board[left[0]][left[1]], board[left[0]][left[1] + 1] = board[left[0]][left[1] + 1], board[left[0]][left[1]]
             left_tile = TileBoard(board, left)
-            left_move = node.path + ["L"]
-            f_n = len(left_move) + left_tile.heuristic(goal)
-            left_tile.fn = f_n
-            left_tile.path = left_move
+            left_tile.path = node.path + ["L"]
+            left_tile.fn = len(left_tile.path) + left_tile.heuristic(goal)
             if make_move(left_tile, explored, frontier):
                 generated += 1
+        # if right position of next blank space is valid
         if right[1] != 3: 
             board = copy.deepcopy(node.board)
             board[right[0]][right[1]], board[right[0]][right[1] - 1] = board[right[0]][right[1] - 1], board[right[0]][right[1]]
             right_tile = TileBoard(board, right)
-            right_move = node.path + ["R"]
-            f_n = len(right_move) + right_tile.heuristic(goal)
-            right_tile.fn = f_n
-            right_tile.path = right_move
+            right_tile.path = node.path + ["R"]
+            right_tile.fn = len(right_tile.path) + right_tile.heuristic(goal)
             if make_move(right_tile, explored, frontier):
                 generated += 1
+        # if up position of next blank space is valid
         if up[0] != -1:
             board = copy.deepcopy(node.board)
             board[up[0]][up[1]], board[up[0] + 1][up[1]] = board[up[0] + 1][up[1]], board[up[0]][up[1]]
             up_tile = TileBoard(board, up)
-            up_move = node.path + ["U"]
-            f_n = len(up_move) + up_tile.heuristic(goal)
-            up_tile.fn = f_n
-            up_tile.path = up_move
+            up_tile.path = node.path + ["U"]
+            up_tile.fn = len(up_tile.path) + up_tile.heuristic(goal)
             if make_move(up_tile, explored, frontier):
                 generated += 1
+        # if down position of next blank space is valid
         if down[0] != 3: 
             board = copy.deepcopy(node.board)
             board[down[0]][down[1]], board[down[0] - 1][up[1]] = board[down[0] - 1][down[1]], board[down[0]][down[1]]
             down_tile = TileBoard(board, down)
-            down_move = node.path + ["D"]
-            f_n = len(down_move) + down_tile.heuristic(goal)
-            down_tile.fn = f_n
-            down_tile.path = down_move
+            down_tile.path = node.path + ["D"]
+            down_tile.fn = len(down_tile.path) + down_tile.heuristic(goal)
             if make_move(down_tile, explored, frontier):
                 generated += 1
 
 def main():
+	# ask user for file name
     input_file = input("Enter a file name: ")
     file = None
+    # attempt to open file
+    # if file not found, report to user
     try:
         file = open(input_file, "r")
     except Exception:
@@ -164,9 +180,12 @@ def main():
     out_file.write("\n")
     out_file.write(str(goal_state))
     out_file.write("\n")
-    out_file.write(str(len(node.path)) + "\n")
-    out_file.write(str(generated) + "\n")
-    out_file.write(" ".join(node.path) + "\n")
+    if node is None:
+    	out_file.write("Failure\n")
+    else:
+	    out_file.write(str(len(node.path)) + "\n")
+	    out_file.write(str(generated) + "\n")
+	    out_file.write(" ".join(node.path) + "\n")
     out_file.close()
 
 main()
